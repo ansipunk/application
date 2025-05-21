338e9064e9cbee856c2999d084c94716cd1412d4
ansipunk <kysput@gmail.com>
2025-05-22 01:39:55 +0500

    Add tags to endpoints

--- application/web/api/__init__.py
+++ application/web/api/__init__.py
@@ -6,6 +6,20 @@ from .organization import router as organization_router
 
 router = fastapi.APIRouter()
 
-router.include_router(building_router, prefix="/buildings")
-router.include_router(activity_router, prefix="/activities")
-router.include_router(organization_router, prefix="/organizations")
+router.include_router(
+    building_router,
+    prefix="/buildings",
+    tags=["buildings"],
+)
+
+router.include_router(
+    activity_router,
+    prefix="/activities",
+    tags=["activities"],
+)
+
+router.include_router(
+    organization_router,
+    prefix="/organizations",
+    tags=["organizations"],
+)
